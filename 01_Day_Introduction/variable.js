bad614cd7de9d1ca37bc974416b9f6f735aa4e21
AmitxParmar <amitparmar901@gmail.com>
2023-02-22 15:38:02 +0530

    add html and variable example in day01

---
+++ 01_Day_Introduction/variable.js
@@ -0,0 +1,15 @@
+// Declare different variables of different data types
+let firstName = "Amit" // first name of the person
+let lastName = "Parmar"
+let country = "India" // country
+let city = "Savarkundla, Saurashtra, Gujarat" // location
+let age = 100
+let isMarried = false
+
+// Declaring variables with number values
+const gravity = 9.81 // earth gravity in m/s2
+const boilingPoint = 100 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constants
+
+// Variables can also be declaring in one line separated by comma
+let name = 'Raphael', job = "Teacher", live = 'UK'
