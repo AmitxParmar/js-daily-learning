26195301481991c90787b6ee297787307eb44839
AmitxParmar <amitparmar901@gmail.com>
2023-02-27 17:25:36 +0530

    add string method practice

---
+++ 02_Day_Data_types/string_methods/replace.js
@@ -0,0 +1,7 @@
+// replace(): takes to parameters the old substring and new substring.
+// string.replace(oldSubstring, newSubstring)
+
+let string = '30 Days Of JavaScript';
+console.log(string.replace('JavaScript', 'Python')) // 30 Days Of Python
+let country = 'Finland';
+console.log(country.replace("Finland", "Noman")) // Nomanland
